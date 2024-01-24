3ff364255f50623de660defe673b8bb0a807fce8
Daniel Kao <dack8@outlook.com>
2024-01-23 16:04:28 -0800

    add things

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -2,19 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class SceneChange : MonoBehaviour
 {
-    int position;
     // Start is called before the first frame update
     void Start()
     {
-        position = 0;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(position, position, 0);
-        position += 1;
+        
     }
 }
