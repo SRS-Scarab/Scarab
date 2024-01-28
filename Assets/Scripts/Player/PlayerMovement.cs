4fc69078af06137c7d69e04dc9a4f945a52bb9c4
Daniel Kao <dack8@outlook.com>
2024-01-27 16:33:12 -0800

    add basic environment scene

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public PlayerHP playerHP;
+    public Rigidbody rb;
     void Start()
     {
         Physics2D.gravity = Vector2.zero;
+        rb = GetComponent<Rigidbody>();
+
     }
 
     void Update()
     {
-        playerHP.damage(1);
+        UpdateMovement();
+    }
+    private void UpdateMovement()
+    {
+        List<string> buttonNames = new List<string>() { "Left", "Right", "Up", "Down" };
+        if (Input.GetButton("Left"))
+            rb.velocity = Vector3Int.left;
+        else if (Input.GetButton("Right"))
+            rb.velocity = Vector3Int.right;
+        else if (Input.GetButton("Up"))
+            rb.velocity = Vector3Int.up;
+        else if (Input.GetButton("Down"))
+            rb.velocity = Vector3Int.down;
     }
 }
