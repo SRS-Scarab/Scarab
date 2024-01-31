4835de9c134f46d89499b5f552ec147ff8dc7a9f
Daniel Kao <dack8@outlook.com>
2024-01-30 16:58:53 -0800

    fix main world for demo

--- Assets/Scripts/Interactions/DialogueInteraction.cs
+++ Assets/Scripts/Interactions/DialogueInteraction.cs
@@ -6,22 +6,44 @@ using Yarn.Unity;
 public class DialogueInteraction : MonoBehaviour
 {
     [Header("Dependencies")]
-    
+
     [SerializeField] private DialogueRunner? dialogueRunner;
     [SerializeField] private Interactable? interactable;
+    [SerializeField] private GameObject? characters;
+    public void Start()
+    {
+        if (characters != null) characters.SetActive(false);
+    }
 
     private void OnEnable()
     {
-        if (interactable != null) interactable.OnInteract += OnTriggerDialogue;
+        if (interactable != null)
+        {
+            interactable.OnInteract += OnTriggerDialogue;
+            if (dialogueRunner != null) dialogueRunner.onDialogueComplete.AddListener(OnDialogueComplete);
+        }
     }
 
     private void OnDisable()
     {
-        if (interactable != null) interactable.OnInteract -= OnTriggerDialogue;
+        if (interactable != null)
+        {
+            interactable.OnInteract -= OnTriggerDialogue;
+            if (dialogueRunner != null) dialogueRunner.onDialogueComplete.RemoveListener(OnDialogueComplete);
+        }
     }
 
     private void OnTriggerDialogue(object sender, EventArgs args)
     {
-        if (dialogueRunner != null) dialogueRunner.StartDialogue(dialogueRunner.startNode);
+        if (dialogueRunner != null)
+        {
+            dialogueRunner.StartDialogue(dialogueRunner.startNode);
+            if (characters != null) characters.SetActive(true);
+        }
+    }
+
+    private void OnDialogueComplete()
+    {
+        if (characters != null) characters.SetActive(false);
     }
 }
