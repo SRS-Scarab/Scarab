98cd074a13af3cc933ad0229a0d8240435662a78
Daniel Kao <dack8@outlook.com>
2024-02-27 15:34:33 -0800

    flesh out 3d scene

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,40 +4,43 @@ using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public ActionsVariable actionsVar;
-    public Rigidbody2D rb;
-    public float speed;
-    public float sprintSpeed;
+  public ActionsVariable actionsVar;
+  public Rigidbody rb;
+  public float speed;
+  public float sprintSpeed;
 
-    [SerializeField] private bool isSprinting;
-    private InputAction _moveAction;
-    
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        rb.gravityScale = 0;
-        _moveAction = actionsVar.Provide().Gameplay.Move;
-        actionsVar.Provide().Gameplay.Sprint.performed += OnBeginSprint;
-        actionsVar.Provide().Gameplay.Sprint.canceled += OnEndSprint;
-    }
+  [SerializeField] private bool isSprinting;
+  private InputAction _moveAction;
 
-    void Update()
-    {
-        UpdateMovement(isSprinting ? sprintSpeed : speed);
-    }
-    
-    private void UpdateMovement(float inSpeed)
-    {
-        rb.velocity = _moveAction.ReadValue<Vector2>().normalized * inSpeed;
-    }
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+    _moveAction = actionsVar.Provide().Gameplay.Move;
+    actionsVar.Provide().Gameplay.Sprint.performed += OnBeginSprint;
+    actionsVar.Provide().Gameplay.Sprint.canceled += OnEndSprint;
+  }
 
-    private void OnBeginSprint(InputAction.CallbackContext context)
-    {
-        isSprinting = true;
-    }
+  void Update()
+  {
+    UpdateMovement(isSprinting ? sprintSpeed : speed);
+  }
 
-    private void OnEndSprint(InputAction.CallbackContext context)
-    {
-        isSprinting = false;
-    }
+  private void UpdateMovement(float inSpeed)
+  {
+    Vector2 actions = _moveAction.ReadValue<Vector2>();
+    Vector3 thing = Quaternion.AngleAxis(-45, Vector3.up) * new Vector3(actions.x, 0, actions.y);
+    Vector3 downward = Vector3.up * rb.velocity.y;
+    rb.velocity = thing.normalized * inSpeed + downward;
+    // rb.velocity = _moveAction.ReadValue<Vector2>().normalized * inSpeed;
+  }
+
+  private void OnBeginSprint(InputAction.CallbackContext context)
+  {
+    isSprinting = true;
+  }
+
+  private void OnEndSprint(InputAction.CallbackContext context)
+  {
+    isSprinting = false;
+  }
 }
