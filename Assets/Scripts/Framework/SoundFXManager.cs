773b658ef3ac3e500b6f34126855e47ebc8d5a9f
Daniel Kao <dack8@outlook.com>
2024-05-20 17:02:41 +0800

    fix sound system and add system for playing audio during dialogue

--- Assets/Scripts/Framework/SoundFXManager.cs
+++ Assets/Scripts/Framework/SoundFXManager.cs
@@ -9,18 +9,18 @@ public class SoundFXManager : MonoBehaviour
     [SerializeField] private AudioSource soundFXObject;
     private void Awake()
     {
-        if (instance ==null) instance = this;
+        if (instance == null) instance = this;
     }
 
-    public void PlaySound(AudioClip audioClip, Transform transform, float volume)
+    public void PlaySound(AudioClip audioClip, Vector3 position = default, float volume = 1.0f)
     {
-        AudioSource audioSource = Instantiate(soundFXObject, transform.position, Quaternion.identity);
+        AudioSource audioSource = Instantiate(soundFXObject, position, Quaternion.identity);
         audioSource.clip = audioClip;
         audioSource.volume = volume;
         audioSource.Play();
-        Destroy(audioSource, audioSource.clip.length);
+        Destroy(audioSource.gameObject, audioSource.clip.length);
     }
-    
+
     public void PlayRandomSound(AudioClip[] audioClip, Transform transform, float volume)
     {
         int rand = Random.Range(0, audioClip.Length);
@@ -28,6 +28,6 @@ public class SoundFXManager : MonoBehaviour
         audioSource.clip = audioClip[rand];
         audioSource.volume = volume;
         audioSource.Play();
-        Destroy(audioSource, audioSource.clip.length);
+        Destroy(audioSource.gameObject, audioSource.clip.length);
     }
 }
